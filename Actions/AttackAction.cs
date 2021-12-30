24ad94fb2a97121d8e16b436e2a46f23bc3281b2
schlenkerc18 <schlenkerc18@hanover.edu>
2021-12-30 10:38:39 -0500

    moved DealHitDamage() and RemoveCharacterFromParty() methods to AttackAction class

--- Actions/AttackAction.cs
+++ Actions/AttackAction.cs
@@ -9,25 +9,6 @@ namespace FinalBattle.Actions
 {
     public class AttackAction : IAction
     {
-        public void PunchAttack(Party friends, Party enemies)
-        {
-            Console.WriteLine($"{friends.characters[0]._name} used Punch on {enemies.characters[0]._name}.");
-            friends.characters[0].DealHitDamage(friends, enemies, ActionType.Punch);
-            //return AttackType.Punch;
-        }
-
-        public void UnravelingAttack(Party friends, Party enemies)
-        {
-            Console.WriteLine($"{friends.characters[0]._name} used Unraveling on {enemies.characters[0]._name}.");
-            friends.characters[0].DealHitDamage(friends, enemies, ActionType.Unraveling);
-        }
-
-        public void BoneCrunchAttack(Party friends, Party enemies)
-        {
-            Console.WriteLine($"{friends.characters[0]._name} used Bone Crunch on {enemies.characters[0]._name}.");
-            friends.characters[0].DealHitDamage(friends, enemies, ActionType.BoneCrunch);
-        }
-
         public void ComputerAction(Party friends, Party enemies, ActionType action)
         {
             if (action == ActionType.Punch) PunchAttack(friends, enemies);
@@ -40,6 +21,58 @@ namespace FinalBattle.Actions
             if (action == ActionType.Punch) PunchAttack(friends, enemies);
             else if (action == ActionType.Unraveling) UnravelingAttack(friends, enemies);
             else BoneCrunchAttack(friends, enemies);
+        }
+
+        public void PunchAttack(Party friends, Party enemies)
+        {
+            Console.WriteLine($"{friends.characters[0]._name} used Punch on {enemies.characters[0]._name}.");
+            DealHitDamage(friends, enemies, ActionType.Punch);
+
+        }
+
+        public void UnravelingAttack(Party friends, Party enemies)
+        {
+            Console.WriteLine($"{friends.characters[0]._name} used Unraveling on {enemies.characters[0]._name}.");
+            DealHitDamage(friends, enemies, ActionType.Unraveling);
+        }
+
+        public void BoneCrunchAttack(Party friends, Party enemies)
+        {
+            Console.WriteLine($"{friends.characters[0]._name} used Bone Crunch on {enemies.characters[0]._name}.");
+            DealHitDamage(friends, enemies, ActionType.BoneCrunch);
+        }
+
+        
+
+        public void DealHitDamage(Party friends, Party enemies, ActionType action)
+        {
+            int hitDamage = 0;
+            Random random = new Random();
+
+            hitDamage = action switch
+            {
+                ActionType.BoneCrunch => random.Next(2),
+                ActionType.Unraveling => random.Next(3),
+                ActionType.Punch => 1
+            };
+
+            if (enemies.characters[0]._currentHealth - hitDamage <= 0)
+            {
+                enemies.characters[0]._currentHealth = 0;
+            }
+            else enemies.characters[0]._currentHealth -= hitDamage;
+
+            Console.WriteLine($"The attack dealt {hitDamage} damage to {enemies.characters[0]._name}.");
+            Console.WriteLine($"{enemies.characters[0]._name} is now at {enemies.characters[0]._currentHealth}/{enemies.characters[0]._maxHealth}");
+
+            if (enemies.characters[0]._currentHealth == 0) RemoveCharacterFromParty(friends, enemies, enemies.characters[0]);
+        }
+
+        public void RemoveCharacterFromParty(Party friends, Party enemies, Character character)
+        {
+            enemies.characters.Remove(character);
+            Console.WriteLine();
+            Console.WriteLine($"{friends.characters[0]._name} killed {character._name}");
         }
     }
 }
