148b3bd624b308483b9df005015ba588655f09eb
Christopher Schlenker <Christopher.Schlenker@covetrus.com>
2021-12-30 15:07:06 -0500

    now showing status of round after each turn

--- Game.cs
+++ Game.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using FinalBattle.Menus;
+using FinalBattle;
 
 namespace FinalBattle
 {
@@ -80,10 +81,13 @@ namespace FinalBattle
                     Console.WriteLine($"It is {goodGuys.characters[playerTurn]._name}'s turn.");
 
                     // this gets menu and then allows player to choose action
-                    menu.GetMenuItems(goodGuys, badGuys);
+                    menu.GetMenuItems(goodGuys, badGuys);                    
 
                     // increment turn before checking if round is over
                     _turn++;
+
+                    // show round status after attack
+                    ShowRoundStatus(goodGuys, badGuys, GetTurn(_turn));
 
                     // check if action eliminated enemy team
                     IsRoundOver(goodGuys, badGuys);
@@ -93,24 +97,28 @@ namespace FinalBattle
                     Thread.Sleep(500);
                 }
 
+                
+
                 if (GetTurn(_turn) == "Monsters")
                 {
                     // Console.WriteLine($"Number of badGuys: {badGuys.characters.Count}");
                     Console.WriteLine($"It is {badGuys.characters[playerTurn]._name}'s turn.");
 
                     // this gets menu and then allows player to choose action
-                    menu.GetMenuItems(badGuys, goodGuys);
+                    menu.GetMenuItems(badGuys, goodGuys);                   
 
                     // increment turn before checking if round is over
                     _turn++;
+
+                    // show round status after attack
+                    ShowRoundStatus(goodGuys, badGuys, GetTurn(_turn));
 
                     // check if action eliminated enemy team
                     IsRoundOver(goodGuys, badGuys);
                     if (_roundOver) break;
 
                     Console.WriteLine();
-                    Thread.Sleep(500);
-                    
+                    Thread.Sleep(500);     
                 }
             }
 
@@ -159,6 +167,22 @@ namespace FinalBattle
             }
 
             else _gameOver = false;
+        }
+
+        public void ShowRoundStatus(Party goodGuys, Party badGuys, string turn)
+        {
+            Console.WriteLine();
+            Console.WriteLine("======================Battle======================");
+            if (turn == "Heroes") Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{goodGuys.characters[0]._name}        ( {goodGuys.characters[0]._currentHealth}/{goodGuys.characters[0]._maxHealth} )");
+            Console.ResetColor();
+            Console.WriteLine("------------------------VS------------------------");
+
+            if (turn == "Monsters") Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < badGuys.characters.Count; i++)
+                Console.WriteLine($"                           {badGuys.characters[i]._name}        ( {badGuys.characters[i]._currentHealth}/{badGuys.characters[i]._maxHealth} )");
+
+            Console.ResetColor();
         }
     }
 }
